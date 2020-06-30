fc06d2b70a4666a82728113ffaad02333ac1fdd3
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-06-30 11:14:48 -0300

    PartI Done

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  const icons = document.querySelectorAll('.icon');
+  icons.forEach(icon => {
+    icon.addEventListener('click', selectIcon);
+  }); 
+}
+
+function selectIcon(ev) {
+  
+}
